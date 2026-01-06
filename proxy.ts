dba8669b79056bf1d14fb0b338d3232d96c1f248
Viraj <mviraj975@gmail.com>
2026-01-06 11:28:48 +0530

    proxy

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt"
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const path = req.nextUrl.pathname
 
   // 1. PUBLIC ROUTE EXCEPTION
