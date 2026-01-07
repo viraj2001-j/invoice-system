346cf39ab8c89c04a0d519e804e3fd9dbd01bb37
Viraj <mviraj975@gmail.com>
2026-01-07 23:29:58 +0530

    also

--- app/dashboard/invoices/payment/PaymentsPage.tsx
+++ app/dashboard/invoices/payment/PaymentsPage.tsx
@@ -76,7 +76,7 @@ export default function PaymentsPage({ invoices = [] }: { invoices: any[] }) {
 
   return (
     <div className="flex min-h-screen bg-slate-50">
-      <Sidebar />
+      {/* <Sidebar /> */}
 
       <main className="flex-1 overflow-y-auto pt-16 md:pt-0">
         <div className="p-8 space-y-8">
